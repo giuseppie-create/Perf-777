3383b0866836a2e6dbee724ff5a2160dbfbb1d51
giuseppie-create <giuseppie@tiscali.it>
2026-06-29 14:04:21 +0800

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-var CACHE='perf777-v53';
+var CACHE='perf777-v54';
 var ASSETS=['./','./index.html','./manifest.json','./apple-touch-icon.png','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',function(e){
   self.skipWaiting();
